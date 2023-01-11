ac832e152b765d3a2c2a80eba5fe703979ef4689
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-01-11 19:03:01 +1100

    Added img and js

--- vue/game/script.js
+++ vue/game/script.js
@@ -1,2 +1,31 @@
-console.log('Game')
+const Gm=[
+    'persona1.png',
+    'persona2.png',
+    'persona3.png',
+    'persona4.png',
+    'persona5.png',
+    'persona6.png',
+    'persona7.png',
+    'persona8.png',
+    'persona9.png',
+    'persona10.png',
+    'persona11.png',
+    'persona12.png',
+];
 
+const gameField = document.getElementById('game-field');
+
+const bannerGm ='screen.jpg';
+const banner = document.createElement('img');
+banner.setAttribute('src', 'img/screen.jpg');
+banner.classList.add('banner');
+
+const gameHeader = document.getElementById('game-header');
+//gameHeader.after(banner);
+
+function handleStartButtonClick() {
+   gameField.innerHTML = '<div class="card"></div>';
+}
+
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', handleStartButtonClick);
