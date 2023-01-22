254d61ff02aa741ef69277559a6489e922d92871
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-01-22 22:18:10 +1100

    Added icon,fixed

--- vue/mexico/src/components/PageFooter.vue
+++ vue/mexico/src/components/PageFooter.vue
@@ -1,10 +1,18 @@
 <template>
   <footer>
     <div class="communications">
-      <a href="https://twitter.com" target="_blank"><div class="twitter"></div></a>
-      <button> VKontakte</button>
-      <button> Facebook</button>
-      <a href="https://www.youtube.com/watch?v=gX2-TzGPXoI">YouTube</a>
+      <a href="https://viber.com" target="_blank">
+        <div class="viber"></div>
+      </a>
+      <a href="https://VK.com" target="_blank">
+        <div class="VK"></div>
+      </a>
+      <a href="https://telegram.org>" target="_blank">
+        <div class="telegram"></div>
+      </a>
+      <a href="https://www.youtube.com/watch?v=gX2-TzGPXoI" target="_blank">
+        <div class="YouTube"></div>
+      </a>
     </div>
   </footer>
 </template>
@@ -30,10 +38,31 @@ footer {
   justify-content: space-around;
 }
 
-.twitter {
+.VK {
   width: 60px;
   height: 60px;
-	background-image: url('@/assets/twit.png');
+  background-image: url('@/assets/vk.png');
+  background-size: contain;
+}
+
+.viber {
+  width: 60px;
+  height: 60px;
+  background-image: url('@/assets/viber.png');
+  background-size: contain;
+}
+
+.telegram {
+  width: 60px;
+  height: 60px;
+  background-image: url('@/assets/teleg.png');
+  background-size: contain;
+}
+
+.YouTube {
+  width: 60px;
+  height: 60px;
+  background-image: url('@/assets/youtub.png');
   background-size: contain;
 }
 </style>
