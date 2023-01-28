ef3247ab45a2b87cbc799db4de4aa2e45b47d384
Alexey Ushakov <75662211+rr-alexey-ushakov@users.noreply.github.com>
2023-01-28 16:57:39 +0700

    Added skate

--- mouse-game/script.js
+++ mouse-game/script.js
@@ -4,40 +4,38 @@ window.onload = () => {
     console.log('window loaded')
 
     const mouse = document.getElementById('mouse')
+    const points = document.getElementById('points')
 
     console.log('mouse:', mouse)
 
-    window.addEventListener('keypress', () => {
-        console.log('jupm')
+    window.addEventListener('keypress', (e) => {
+        console.log('jump e:', e)
+        console.log(mouse.classList.length) // .includes('jump')
 
-        mouse.classList.add('jump')
+        if (e.keyCode === 32 && !mouse.classList.length) {
+            mouse.classList.add('jump')
 
-        setTimeout(() => {
-            mouse.classList.remove('jump')
-        }, 800)
+            setTimeout(() => {
+                mouse.classList.remove('jump')
+            }, 800)
+
+            points.innerText = parseInt(points.innerText) + 1
+        }
+
     }, false)
 
     const start = document.getElementById('start')
 
     function startGame() {
+        start.classList.add('hidden')
         document.querySelector('.game').classList.add('active')
+        document.querySelector('#cheese').classList.add('active')
+        document.querySelector('#cactus').classList.add('active')
     }
 
-    start.addEventListener('click', () => {
-        console.log('start')
+    // start.addEventListener('click', () => {
+    //     startGame()
+    // }, false)
 
-        startGame()
-    }, false)
-
-
-        document.querySelector('.cheese').classList.add('active')
-    }
-
-    start.addEventListener('click', () => {
-        console.log('start')
-
-        startGame()
-    }, false)
-
-
-
+    start.addEventListener('click', () => startGame(), false)
+}
