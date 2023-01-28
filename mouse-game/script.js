4ed7898790384228669c2e469e5ee008ca0df4a5
Alexey Ushakov <75662211+rr-alexey-ushakov@users.noreply.github.com>
2023-01-28 17:57:30 +0700

    Added second cactus

--- mouse-game/script.js
+++ mouse-game/script.js
@@ -4,13 +4,24 @@ window.onload = () => {
     console.log('window loaded')
 
     const mouse = document.getElementById('mouse')
+    const mouseOnSkate = document.getElementById('mouse-on-skate')
     const points = document.getElementById('points')
 
     console.log('mouse:', mouse)
 
     window.addEventListener('keypress', (e) => {
-        console.log('jump e:', e)
+        console.log('Keyboard event:', e)
         console.log(mouse.classList.length) // .includes('jump')
+
+        if (e.keyCode === 119) {
+            console.log('up')
+            mouseOnSkate.classList.add('up')
+            mouseOnSkate.classList.remove('down')
+        } else if (e.keyCode === 115) {
+            console.log('down')
+            mouseOnSkate.classList.add('down')
+            mouseOnSkate.classList.remove('up')
+        }
 
         if (e.keyCode === 32 && !mouse.classList.length) {
             mouse.classList.add('jump')
@@ -30,6 +41,7 @@ window.onload = () => {
         start.classList.add('hidden')
         document.querySelector('.game').classList.add('active')
         document.querySelector('#cheese').classList.add('active')
+        document.querySelector('#cactus-up').classList.add('active')
         document.querySelector('#cactus').classList.add('active')
     }
 
