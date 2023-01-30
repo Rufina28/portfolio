e75d21ed87afe544069c5c363af57132597e6fdb
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-01-30 22:53:53 +1100

    Added a nested text

--- vue/mexico/src/components/PhotoPage.vue
+++ vue/mexico/src/components/PhotoPage.vue
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="card"><img src="@/assets/galery1.jpg" alt="">
 			<div class="name">
-				<button> Чичен-Ица</button>
+				<a href="#" class="circle">Чичен-Ица<span class="circle"> Один из самых больших, величественных и наиболее хорошо восстановленных монументов эпохи майя. Знаменитые пирамиды и храмы Чичен-Ицы — самые известные из древних памятников полуострова Юкатан. Прогуливаясь среди каменных платформ, пирамид и церемониальных залов, начинаешь лучше понимать и ценить высокую цивилизацию далекой древности.</span></a>
 			</div>
 		</div>
 		<div class="card"><img src="@/assets/galery2.jpg" alt="">
@@ -47,7 +47,8 @@ export default {
 }
 
 .name {
-	margin-top: 50px;
+	margin-top: 20px;
+	display: inline-block;
 }
 
 .container {
@@ -80,10 +81,78 @@ button {
 	margin-top: 10px;
 }
 
+.circle {
+  display: block;
+  /*position: absolute; */
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 175px;
+  height: 60px;
+  padding-top: 30px;
+  border-radius: 300px;
+  border-width: 4px;
+  border-style: solid;
+  text-decoration: none;
+  text-align: center;
+  font-size: 25px;
+  box-shadow: 2px 2px 7px hsla(0, 0%, 4%, 0.95);
+  text-shadow: 0 1px 0 rgba(9 9 9 / 85%);
+  letter-spacing: 1;
+  transition: all .25s ease-in-out;
+}
+
+.circle:hover {
+  z-index: 2;
+  width: 400px;
+  height: 400px;
+  left: -15%;
+  top: -15%;
+  padding-top: 38px;
+  font-size: 33px;
+  border: 10px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .3);
+  transform: rotate(-360deg);
+}
+
+  a.circle {
+	color: black;
+}
+
+.circle span.circle {
+  display: block;
+  opacity: 0;
+  padding: 40% 20% 0 20%;
+  font-size: 1px;
+  line-height: 1.5;
+  border: none;
+  color: rgba(0, 0, 0, 1);
+  transition: all .5s ease-in-out;
+}
+
+.circle span:hover {
+  opacity: .85;
+  font-size: 20px;
+  text-shadow: 0 1px 1px rgba(0, 0, 0, .5);
+}
+
+.circle span {
+  background: rgba(102, 204, 0, .7);
+}
+/*
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-300deg);
+  }
+}
+
 @media screen and (max-width: 1800px) {
   .container {
     grid-template-columns: auto;
   }
-}
+}*/
 
 </style>
