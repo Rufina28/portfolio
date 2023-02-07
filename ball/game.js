e57701cf06742ab70bd05d0f04611e9e224d39aa
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-02-07 23:15:35 +1100

    Added platform

--- ball/game.js
+++ ball/game.js
@@ -1,14 +1,46 @@
 let game = {
-    start: function() {
-     this.ctx = document.getElementById("mycanvas").getContext("2d");   
-    let background = new Image();
-    background.src = "img/background.png";
-    window.requestAnimationFrame(() => {
-        this.ctx.drawImage(background, 0, 0);
-    });
+    ctx: null,
+    sprites: {
+        background: null,
+        ball: null,
+        platform: null
+    },
+    init: function () {
+        this.ctx = document.getElementById("mycanvas").getContext("2d");
+    },
+    preolad(callback) {
+        let loaded = 0;
+        let required = Object.keys(this.sprites).length;
+
+        for (let key in this.sprites) {
+            this.sprites[key] = new Image();
+            this.sprites[key].src = "img/" + key + ".png";
+            this.sprites[key].addEventListener("load", () => {
+                ++loaded;
+                if (loaded >= required) {
+                    callback();
+                }
+            });
+        }
+    },
+    run() {
+        window.requestAnimationFrame(() => {
+            this.render();
+        });
+    },
+    render() {
+        this.ctx.drawImage(this.sprites.background, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0);
+        this.ctx.drawImage(this.sprites.platform, 0, 0);
+    },
+    start: function () {
+        this.init();
+        this.preolad(() => {
+            this.run();
+        });
     }
 };
 
-window.addEventListener("load",() => {
+window.addEventListener("load", () => {
     game.start();
 });
