b30a94a47fc69bab08da2e06c915c63bf48ab54d
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-02-07 23:38:38 +1100

    Fixed location of objects

--- ball/game.js
+++ ball/game.js
@@ -1,5 +1,7 @@
 let game = {
     ctx: null,
+    platform: null,
+    ball: null,
     sprites: {
         background: null,
         ball: null,
@@ -30,8 +32,8 @@ let game = {
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y,  this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
     start: function () {
         this.init();
@@ -41,6 +43,17 @@ let game = {
     }
 };
 
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+    };
+
+game.platform = {
+x: 280,
+y: 300
+};
 window.addEventListener("load", () => {
     game.start();
 });
