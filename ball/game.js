f20fb20b8a03a15ba3870b402a6ffe2a5508a5cd
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-02-08 00:18:49 +1100

    Added blocks

--- ball/game.js
+++ ball/game.js
@@ -2,10 +2,14 @@ let game = {
     ctx: null,
     platform: null,
     ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
-        platform: null
+        platform: null,
+        block: null
     },
     init: function () {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
@@ -13,16 +17,27 @@ let game = {
     preolad(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
+        let onImageLoand = () => {
+        ++loaded;
+        if (loaded >= required) {
+            callback();
+        }
+    };
 
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
-            this.sprites[key].addEventListener("load", () => {
-                ++loaded;
-                if (loaded >= required) {
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener("load",onImageLoand);     
+        }
+    },
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x:64 * col + 65,
+                    y:24 * row + 35
+                });
+            }
         }
     },
     run() {
@@ -34,10 +49,17 @@ let game = {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y,  this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+       this.renderBlocks();
     },
+        renderBlocks() {
+      for (let block of this.blocks) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
+      }, 
     start: function () {
         this.init();
         this.preolad(() => {
+            this.create();  
             this.run();
         });
     }
