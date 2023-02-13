3005f5ad1ec8199dad60ad80c238f642772da14a
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-02-13 17:46:40 +1100

    Added snake movement

--- snake/js/game.js
+++ snake/js/game.js
@@ -84,16 +84,28 @@ fitHeight(data) {
             this.sprites[key].addEventListener('load', onAssetLoad);
         }
     },
-    run() {
-        console.log('run')
-        this.board.create();
-        this.snake.create();
-
-        window.requestAnimationFrame(() => {
-            this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width ) / 2, (this.height - this.sprites.background.height));
-            this.board.render();
-            this.snake.render();
+    create() {
+this.board.create();
+this.snake.create();
+    },
+    render() {
+        window.requestAnimationFrame(() => { 
+        this.ctx.clearRect(0,0, this.width, this.height);
+        this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width ) / 2, (this.height - this.sprites.background.height));
+        this.board.render();
+        this.snake.render();
         });
+    },
+    update () {
+        this.snake.move();
+        this.render();
+    },
+    run() {
+        
+        this.create();
+setInterval(() => {
+    this.update();
+}, 150);
     }
 };
 
--- snake/js/snake.js
+++ snake/js/snake.js
@@ -18,5 +18,18 @@ game.snake = {
         this.cells.forEach(cell => {
             this.game.ctx.drawImage(this.game.sprites.body, cell.x, cell.y);
         });
+    },
+    move() {
+        let cell = this.getNextCell();
+        if (cell) {
+            this.cells.unshift(cell);
+            this.cells.pop();
+        }
+    },
+    getNextCell() {
+        let head = this.cells[0];
+        let row = head.row - 1;
+        let col = head.col;
+        return this.game.board.getCell(row, col);
     }
 };
