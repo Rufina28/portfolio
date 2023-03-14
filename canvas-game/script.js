8cdf7943f849462c74e0cc28caf1f710b33e2bf2
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-03-14 20:20:41 +0700

    Added projectile

--- canvas-game/script.js
+++ canvas-game/script.js
@@ -20,9 +20,37 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity  
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+}
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
+
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
 console.log(player)
+addEventListener('click', (event) => {
+    const projectile = new Projectile(
+        event.clientX, 
+        event.clientY,
+        5,
+        'red',
+        null
+    )
+    projectile.draw()
+
+})
