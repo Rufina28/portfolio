a90c5f06ba5cd2004b03047cbc79b1e272216a87
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-03-18 13:09:45 +0700

    Added Enemy

--- canvas-game/script.js
+++ canvas-game/script.js
@@ -41,20 +41,60 @@ class Projectile {
         this.y = this.y + this.velocity.y
     }
 }
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity  
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
-player.draw()
-
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+setInterval(() => {
+    const x = 100
+    const y = 100
+    const radius = 30
+    const color = 'green'
+    const velocity = {
+        x: 1,
+        y: 1
+    }
+
+enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
     projectiles.forEach((projectile) => {
 projectile.update()
+    })
+
+    enemies.forEach(enemy => {
+        enemy.update()
     })
 }
 
