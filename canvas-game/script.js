54de4c9dcdf2ceab8c91f16bde7dcf0178ff1bb7
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-03-28 12:24:18 +0700

    Fixed

--- canvas-game/script.js
+++ canvas-game/script.js
@@ -72,15 +72,27 @@ const enemies = []
 
 function spawnEnemies() {
     setInterval(() => {
-        console.log(' * spawnEnemies')
-        const x = 100
-        const y = 100
-        const radius = 30
-        const color = 'green'
-        const velocity = {
-            x: 1,
-            y: 1
+        const radius = Math.random() * (30 - 4) + 4
+
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+        y = Math.random() * canvas.height 
+        } else {
+            x = Math.random() * canvas.width 
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
+        
+        const color = 'green'
+        
+        const angle = Math.atan2(canvas.height / 2 - y,
+        canvas.width / 2 - x)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
 
         enemies.push(new Enemy(x, y, radius, color, velocity))
     }, 1000)
