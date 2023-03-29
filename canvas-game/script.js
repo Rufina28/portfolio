76301bb28b41eb18a34234eb8a024b9023d7d124
Rufina28 <120560431+Rufina28@users.noreply.github.com>
2023-03-29 13:46:22 +0700

    Added colorize game

--- canvas-game/script.js
+++ canvas-game/script.js
@@ -66,7 +66,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 20, 'white')
 const projectiles = []
 const enemies = []
 
@@ -85,7 +85,7 @@ function spawnEnemies() {
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
 
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         
         const angle = Math.atan2(canvas.height / 2 - y,
         canvas.width / 2 - x)
@@ -100,10 +100,23 @@ function spawnEnemies() {
 let animationId
 function animate() {
    animationId = requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach((projectile) => {
         projectile.update()
+        //remove from edges of screen
+        if (
+        projectile.x + projectile.radius < 0 ||
+        projectile.x - projectile.radius > canvas.width ||
+
+        projectile.y + projectile.radius < 0 ||
+        projectile.y - projectile.radius > canvas.height
+        ) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
 
     enemies.forEach((enemy, index) => {
@@ -135,8 +148,8 @@ addEventListener('click', (event) => {
         event.clientX - canvas.width / 2
     )
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 6,
+        y: Math.sin(angle) * 6
     }
     projectiles.push(
         new Projectile(canvas.width / 2, canvas.height / 2,
